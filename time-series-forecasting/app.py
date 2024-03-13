e716d806cdf93ae0d1fffb5a5f4ea944ab1558d4
Franziska Greiner <franziska.greiner@ausy-technologies.de>
2024-03-13 19:17:55 +0100

    Update app, tft tbd

--- time-series-forecasting/app.py
+++ time-series-forecasting/app.py
@@ -20,8 +20,8 @@ checkpoint_path = f"{tft_config.get('output_path')}/tft-epoch=09-val_loss=3.16.c
 tft_model = TemporalFusionTransformer.load_from_checkpoint(checkpoint_path)
 exog_variables = ['temperature', 'ghi', 'wind_speed', 'precipitation', 'day_of_week', 'is_holiday', 'season']
 
-de_start_date = datetime.strptime('2022-10-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-se_start_date = datetime.strptime('2021-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+de_start_date = pd.to_datetime('2022-10-01')
+no_start_date = pd.to_datetime('2021-01-01')
 
 
 @app.route('/predict', methods=['POST'])
@@ -36,21 +36,19 @@ def predict():
 
     try:
         data_for_prediction = pd.read_csv(
-            f"{general_config.get('data_path')}/weather_{country}_2024_2025_with_features.csv",
+            f"{general_config.get('data_path')}/weather_time_{country}_2024_2025.csv",
             index_col='date', parse_dates=True
         )
         validation_test_variables = pd.read_csv(
             f"{general_config.get('data_path')}/validation_test_{country}.csv",
             index_col='date', parse_dates=True
         )
-        # data_for_prediction.index = pd.DatetimeIndex(data_for_prediction.index).to_period('H')
     except FileNotFoundError:
         return None, None, f"Data file not found for country {country}"
 
     exogenous_data = pd.concat([validation_test_variables, data_for_prediction[exog_variables]])
     exogenous_data.index = pd.to_datetime(exogenous_data.index)
 
-    # Erstellen eines vollständigen Datumsbereichs
     start_date = exogenous_data.index.min()
     end_date = exogenous_data.index.max()
     full_range = pd.date_range(start=start_date, end=end_date, freq='H')
@@ -63,44 +61,55 @@ def predict():
     else:
         print("Fehlende Zeitpunkte:", missing_times)
 
+    earliest_time = datetime.now() + timedelta(minutes=2)
+    latest_time = due_date - timedelta(minutes=duration)
     if model_type == 'tft':
-        optimal_start_time = find_optimal_start_time(
-            model_type, tft_model, duration, None, None, exogenous_data
+        exogenous_data.reset_index(inplace=True)
+        exogenous_data['date'] = pd.to_datetime(exogenous_data['date'])
+        exogenous_data.loc[:, 'time_idx'] = get_time_index_from_date(exogenous_data['date'], country)
+        earliest_time_idx, latest_time_idx = get_time_indexes(due_date, duration, country)
+        optimal_start_time, lowest_moer_sum, highest_moer_sum = find_optimal_start_time(
+            model_type, tft_model, duration, earliest_time, latest_time, exogenous_data, country
         )
     elif model_type == 'sarimax':
         sarimax_model_path = f"{sarimax_config.get('output_path')}/sarimax_model_{country}.joblib"
         sarimax_model = joblib.load(sarimax_model_path)
-        optimal_start_time = find_optimal_start_time(
-            model_type, sarimax_model, duration, datetime.now(), due_date, exogenous_data
+        optimal_start_time, lowest_moer_sum, highest_moer_sum = find_optimal_start_time(
+            model_type, sarimax_model, duration, earliest_time, latest_time, exogenous_data, country
         )
     else:
         return jsonify("No valid model type specified, using default: tft")
 
-    return jsonify({'optimal_start_time': optimal_start_time})
+    return f'Best start time is {optimal_start_time} with an expected MOER sum of {lowest_moer_sum.round(2)} g/kWh. ' \
+           f'You can save up to {(highest_moer_sum-lowest_moer_sum).round(2)} g/kWh.'
 
 
-def get_time_indexes_and_data(due_date, duration, country):
-    earliest_start_time = datetime.now()
+def get_time_indexes(due_date, duration, country):
+    earliest_start_time = datetime.now() + timedelta(minutes=2)
     latest_start_time = due_date - timedelta(minutes=duration)
 
     if country == 'DE':
         earliest_time_idx = int((earliest_start_time - de_start_date).total_seconds() // 3600)
         latest_time_idx = int((latest_start_time - de_start_date).total_seconds() // 3600)
     elif country == 'SE':
-        earliest_time_idx = int((earliest_start_time - se_start_date).total_seconds() // 3600)
-        latest_time_idx = int((latest_start_time - se_start_date).total_seconds() // 3600)
+        earliest_time_idx = int((earliest_start_time - no_start_date).total_seconds() // 3600)
+        latest_time_idx = int((latest_start_time - no_start_date).total_seconds() // 3600)
 
-    try:
-        data_for_prediction = pd.read_csv(f"{general_config.get('data_path')}/weather_{country}_2024_2025_with_features.csv")
-    except FileNotFoundError:
-        return None, None, f"Data file not found for country {country}"
-
-    return earliest_time_idx, latest_time_idx, data_for_prediction
+    return earliest_time_idx, latest_time_idx
 
 
-def find_optimal_start_time(model_type, model, duration, earliest_time, latest_time, exog_data):
+def get_time_index_from_date(date, country):
+    if country == 'DE':
+        time_idx = ((date - de_start_date).dt.total_seconds() / 3600).astype(int)
+    elif country == 'NO':
+        time_idx = ((date - no_start_date).dt.total_seconds() / 3600).astype(int)
+    return time_idx
+
+
+def find_optimal_start_time(model_type, model, duration, earliest_time, latest_time, exog_data, country):
     best_time_slot = None
     lowest_moer_sum = float('inf')
+    highest_moer_sum = 0
 
     # Iterate over possible start times from now until the latest start time
     daterange = pd.date_range(start=earliest_time, end=latest_time, freq='H')
@@ -113,28 +122,33 @@ def find_optimal_start_time(model_type, model, duration, earliest_time, latest_t
             if moer_sum < lowest_moer_sum:
                 best_time_slot = date
                 lowest_moer_sum = moer_sum
+            elif moer_sum > highest_moer_sum:
+                highest_moer_sum = moer_sum
         else:
-            predicted_moer = predict_with_tft(model, date, duration, exog_data)
+            time_idx = int((date - de_start_date).total_seconds() / 3600)
+            predicted_moer = predict_with_tft(model, time_idx, duration, exog_data, country)
             moer_sum = np.sum(predicted_moer[:duration // 60])  # prediction is hourly and duration is in minutes
 
             if moer_sum < lowest_moer_sum:
                 best_time_slot = date
                 lowest_moer_sum = moer_sum
+            elif moer_sum > highest_moer_sum:
+                highest_moer_sum = moer_sum
 
-    optimal_start_time = exog_data[exog_data.index == best_time_slot].iloc[0]
-    return optimal_start_time.strftime('%Y-%m-%d %H:%M:%S')
+    # optimal_start_time = exog_data[exog_data.index == best_time_slot].iloc[0]
+    return best_time_slot, lowest_moer_sum, highest_moer_sum
 
 
-def predict_with_tft(model, start_time_idx, duration, data):
+def predict_with_tft(model, start_time_idx, duration, data, country):
     scaler = joblib.load(f"{general_config.get('output_path')}feature_scaler.joblib")
-    features_to_normalize = ['temperature', 'ghi', 'precipitation', 'wind_speed']
+    features_to_normalize = ['temperature', 'ghi', 'wind_speed', 'precipitation']
     target_normalizer = GroupNormalizer(groups=["country"], transformation="softplus")
 
     data[features_to_normalize] = scaler.transform(data[features_to_normalize])
-    data = data[data["country"] == "DE"]
+    data["country"] = country
     data['is_holiday'] = data['is_holiday'].astype(str)
-    data['season'] = data['season'].astype('category')
-    data['day_of_week'] = data['day_of_week'].astype('category')
+    data['season'] = data['season'].astype(str)
+    data['day_of_week'] = data['day_of_week'].astype(str)
 
     # Ensure that moer is not part of the data
     if 'moer' in data.columns:
@@ -170,7 +184,7 @@ def predict_with_tft(model, start_time_idx, duration, data):
 
 
 def predict_with_sarimax(model, start_time, end_time, data):
-    exog_data = data.loc[:end_time]
+    exog_data = data.loc[:(end_time + timedelta(hours=1))]
     start = start_time.strftime('%Y-%m-%d %H:%M:%S')
     end = end_time.strftime('%Y-%m-%d %H:%M:%S')
     predicted_moer = model.predict(start=start, end=end, exog=exog_data)
