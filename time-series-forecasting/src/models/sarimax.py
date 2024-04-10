15b9c4a5cfe5c3f365b7f67d3a67677faff14115
Franziska Greiner <franziska.greiner@ausy-technologies.de>
2024-04-10 13:48:43 +0200

    Update sarimax to fix interpolation and transform cyclic features

--- time-series-forecasting/src/models/sarimax.py
+++ time-series-forecasting/src/models/sarimax.py
@@ -12,6 +12,20 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolu
 sarimax_config = get_sarimax_config()
 
 
+def transform_features(weather_time_moer_data):
+    weather_time_moer_data['season_sin'] = np.sin(2 * np.pi * weather_time_moer_data['season'] / 4)
+    weather_time_moer_data['season_cos'] = np.cos(2 * np.pi * weather_time_moer_data['season'] / 4)
+    weather_time_moer_data['hour_of_day_sin'] = np.sin(2 * np.pi * weather_time_moer_data['hour_of_day'] / 24)
+    weather_time_moer_data['hour_of_day_cos'] = np.cos(2 * np.pi * weather_time_moer_data['hour_of_day'] / 24)
+    weather_time_moer_data['day_of_week_sin'] = np.sin(2 * np.pi * weather_time_moer_data['day_of_week'] / 7)
+    weather_time_moer_data['day_of_week_cos'] = np.cos(2 * np.pi * weather_time_moer_data['day_of_week'] / 7)
+    weather_time_moer_data['day_of_year_sin'] = np.sin(2 * np.pi * weather_time_moer_data['day_of_year'] / 365)
+    weather_time_moer_data['day_of_year_cos'] = np.cos(2 * np.pi * weather_time_moer_data['day_of_year'] / 365)
+    cols_to_drop = ['season', 'hour_of_day', 'day_of_week', 'day_of_year']
+    weather_time_moer_data = weather_time_moer_data.drop(cols_to_drop, axis=1)
+    return weather_time_moer_data
+
+
 def normalize_features(weather_time_moer_data):
     scaler = StandardScaler()
     features_to_normalize = ['temperature', 'ghi', 'wind_speed', 'precipitation']
@@ -21,18 +35,25 @@ def normalize_features(weather_time_moer_data):
     return weather_time_moer_data
 
 
-def create_sarimax_datasets(weather_time_moer_data):
-    weather_time_moer_data.set_index('date', inplace=True)
-    weather_time_moer_data.index = pd.to_datetime(weather_time_moer_data.index)
-    weather_time_moer_data = weather_time_moer_data.asfreq('h')
+def interpolate_data(country_data, country):
+    country_data.set_index('date', inplace=True)
+    country_data.index = pd.to_datetime(country_data.index)
+    country_data = country_data.asfreq('h')
 
-    weather_time_moer_data = weather_time_moer_data[(weather_time_moer_data.index >= sarimax_config.get('start_date'))]
-    train_data = weather_time_moer_data[weather_time_moer_data.index <= sarimax_config.get('training_cutoff_date')]
-    validation_data = weather_time_moer_data[weather_time_moer_data.index > sarimax_config.get('training_cutoff_date')]
+    # Set country bc strings cannot be interpolated
+    country_data['country'] = country
+    country_data_complete = country_data.interpolate(method='linear', limit_direction='both')
+    return country_data_complete
+
+
+def create_sarimax_datasets(country_data_complete):
+    country_data_complete = country_data_complete[(country_data_complete.index >= sarimax_config.get('start_date'))]
+    train_data = country_data_complete[country_data_complete.index <= sarimax_config.get('training_cutoff_date')]
+    validation_data = country_data_complete[country_data_complete.index > sarimax_config.get('training_cutoff_date')]
 
     exog_variables = [
-        'temperature', 'ghi', 'wind_speed', 'precipitation', 'hour_of_day', 'day_of_week',
-        'day_of_year', 'is_holiday_or_weekend', 'season'
+        'temperature', 'ghi', 'wind_speed', 'precipitation', 'hour_of_day_sin', 'hour_of_day_cos', 'day_of_week_sin',
+        'day_of_week_cos', 'day_of_year_sin', 'day_of_year_cos', 'is_holiday_or_weekend', 'season_sin', 'season_cos'
     ]
     exog_train = train_data[exog_variables] if not train_data.empty else None
     exog_validation = validation_data[exog_variables] if not validation_data.empty else None
@@ -64,11 +85,7 @@ def create_sarimax_model(train_data, exog_train, config):
     return sarimax_model
 
 
-def calculate_and_plot_metrics(results, train_data, validation_data, exog_validation, country):
-    if not validation_data.empty:
-        val_predictions = results.get_prediction(start=len(train_data),
-                                                 end=len(train_data) + len(validation_data) - 1,
-                                                 exog=exog_validation, dynamic=False).predicted_mean
+def calculate_metrics(validation_data, val_predictions, country):
         # overall metrics
         val_mse = mean_squared_error(validation_data['moer'], val_predictions)
         val_mae = mean_absolute_error(validation_data['moer'], val_predictions)
@@ -83,19 +100,6 @@ def calculate_and_plot_metrics(results, train_data, validation_data, exog_valida
         #     wandb.log({f"{country}_{week}_validation_MSE": metrics['MSE'],
         #                f"{country}_{week}_validation_MAE": metrics['MAE'],
         #                f"{country}_{week}_validation_MAPE": metrics['MAPE']})
-
-        plt.figure(figsize=(10, 5))
-        plt.plot(validation_data.index, validation_data['moer'], label='Actual')
-        plt.plot(validation_data.index, val_predictions, label='Prediction', linestyle='--')
-        plt.xticks(rotation=45)
-        plt.title(f'validation prediction vs. actual ({country})')
-        plt.legend()
-        plt.ylabel('MOER (gCO2/kWh)')
-        plt.tight_layout()
-        val_plot_file_path = f"{wandb.run.dir}/plot_val_{country}.png"
-        plt.savefig(val_plot_file_path)
-        plt.close()
-        wandb.log({f"plot_val_{country}": wandb.Image(val_plot_file_path)})
 
 
 def calculate_weekly_metrics(actual, predicted):
@@ -126,6 +130,21 @@ def calculate_weekly_metrics(actual, predicted):
     return weekly_metrics
 
 
+def plot_evaluation(validation_data, val_predictions, country):
+    plt.figure(figsize=(10, 5))
+    plt.plot(validation_data.index, validation_data['moer'], label='Actual')
+    plt.plot(validation_data.index, val_predictions, label='Prediction', linestyle='--')
+    plt.xticks(rotation=45)
+    plt.title(f'validation prediction vs. actual ({country})')
+    plt.legend()
+    plt.ylabel('MOER (gCO2/kWh)')
+    plt.tight_layout()
+    val_plot_file_path = f"{wandb.run.dir}/plot_val_{country}.png"
+    plt.savefig(val_plot_file_path)
+    plt.close()
+    wandb.log({f"plot_val_{country}": wandb.Image(val_plot_file_path)})
+
+
 def log_diagnostics(results, country):
     fig = results.plot_diagnostics(figsize=(10, 8))
     diagnostics_path = f"{wandb.run.dir}/diagnostics_{country}.png"
@@ -135,19 +154,24 @@ def log_diagnostics(results, country):
 
 def train_sarimax(weather_time_moer_data):
     run = wandb.init(project="tsf_moer_sarimax", config=sarimax_config)
+    weather_time_moer_data = transform_features(weather_time_moer_data)
     weather_time_moer_data = normalize_features(weather_time_moer_data)
 
     for country in ['DE', 'NO']:
         sarimax_country_config = sarimax_config.get(country.lower())
         country_data = weather_time_moer_data.loc[weather_time_moer_data['country'] == country]
-        country_data_complete = country_data.interpolate(method='linear', limit_direction='both')
+        country_data_complete = interpolate_data(country_data, country)
         train_data, validation_data, exog_train, exog_validation = create_sarimax_datasets(country_data_complete)
         # create_auto_arima(train_data, exog_train)
         sarimax_model = create_sarimax_model(train_data, exog_train, sarimax_country_config)
         results = sarimax_model.fit(disp=False)
 
         print(results.summary())
-        calculate_and_plot_metrics(results, train_data, validation_data, exog_validation, country)
+        val_predictions = results.get_prediction(start=len(train_data),
+                                                 end=len(train_data) + len(validation_data) - 1,
+                                                 exog=exog_validation, dynamic=False).predicted_mean
+        calculate_metrics(validation_data, val_predictions, country)
+        plot_evaluation(validation_data, val_predictions, country)
         log_diagnostics(results, country)
 
         model_file_path = f"{wandb.run.dir}/sarimax_model_{country}.joblib"
