1e30dee7f8ada278ce91ed681debd43ad8e8c354
Franziska Greiner <franziska.greiner@ausy-technologies.de>
2024-04-12 20:11:05 +0200

    Add images and tables

---
+++ thesis/code/api-call.m
@@ -0,0 +1,7 @@
+{
+    "model_type": "sarimax",  // "sarimax" or "tft"
+    "prediction_length": 720,  // 168 or 720
+    "duration": 120,  // in minutes
+    "due_date": "2024-02-23 22:00:00",  // format: 'YYYY-MM-DD HH:MM:SS'
+    "country": "DE"  // "DE" or "NO"
+}
