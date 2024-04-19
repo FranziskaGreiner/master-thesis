c6beb26d5339df28f464f79aff2b3cec904b5d76
Franziska Greiner <franziska.greiner@ausy-technologies.de>
2024-04-19 02:43:38 +0200

    Update code, figures and tables

--- thesis/code/sarimax_model-train.m
+++ thesis/code/sarimax_model-train.m
@@ -1,2 +1,5 @@
-sarimax_model = SARIMAX(train_data['moer'], exog=exog_train, order=(p, d, q), seasonal_order=(P, D, Q, s))
+sarimax_model = SARIMAX(train_data['moer'],
+                exog=exog_train,
+                order=(p, d, q),
+                seasonal_order=(P, D, Q, s))
 results = sarimax_model.fit()
